23b55a7a7298af07e3347292a3176c55a601e26c
rinoa349 <chan2s@icloud.com>
2023-05-20 19:53:38 +0900

    ユーザー管理機能

---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname,
+    presence: true
+  validates :family_name,
+    format: { with: /\A([ぁ-んァ-ン一-龥]|ー)+\z/},
+    presence: true
+  validates :first_name,
+    format: { with: /\A([ぁ-んァ-ンー-龥]|ー)+\z/},
+    presence: true
+  validates :family_name_kana,
+    format: { with: /\A([ァ-ン]|ー)+\z/},
+    presence: true
+  validates :first_name_kana,
+    format: { with: /\A([ァ-ン]|ー)+\z/},
+    presence: true
+  validates :birth_day,
+    presence: true
+
+  has_many :items
+  has_many :orders
+end
