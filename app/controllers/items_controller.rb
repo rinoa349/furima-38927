ee52e1a2c16a421262f3113cbb181b4849516743
rinoa349 <chan2s@icloud.com>
2023-06-04 16:14:51 +0900

    (再)商品詳細、(新)情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
   def index
     @items = Item.includes(:user).order("created_at DESC")
   end
@@ -16,12 +17,31 @@ class ItemsController < ApplicationController
     render :new
    end
 
+   def show
+   end
+
+   def edit
+   end
+
+   def update
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path(item_params)
+    else
+      render 'edit'
+    end
+   end
+
    
   end
 
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :item_status_id, :prefecture_id, :category_id, :shipping_cost_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
   
 end
