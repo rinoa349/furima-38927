764b10e1dfacef375a03e31a79e425e7213753dd
rinoa349 <chan2s@icloud.com>
2023-06-19 17:00:01 +0900

    クレジット機能追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
   def create
     @order_form = OrderForm.new(order_params)
     if @order_form.valid?
+      pay_item
       @order_form.save
       redirect_to root_path
     else
@@ -19,11 +20,20 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_form).permit(:post_code, :prefecture_id, :city, :adress, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_form).permit(:post_code, :prefecture_id, :city, :adress, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def item_params
     @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
-  
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
 end
--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,16 +1,17 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :adress, :building_name, :phone_number
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :adress, :building_name, :phone_number, :token
 
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code, format: {with: /\A\d{3}[-]\d{4}\z/}
-    validates :prefecture_id, numericality: { other_than: 1 , message:"can't be blank" }
+    validates :post_code, format: {with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Include hyphen(-)'}
+    validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
     validates :city
     validates :adress
     validates :building_name
-    validates :phone_number, format: {with: /\A\d{10,11}\z/}
+    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: 'is invalid'}
+    validates :token
   end
 
   def save
